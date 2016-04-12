6ffa1ef5d853035013e21e7c4b2bb696ee20a0d1
jorgeaber <jorgeaber@gmail.com>
2016-04-12 18:45:24 -0300

    Add create event functionality

---
+++ test_events.py
@@ -0,0 +1,11 @@
+from app import Event
+import requests
+
+BASE_URL = 'http://127.0.0.1:5000'
+
+def test_create():
+    title = 'Lorem ipsum amet'
+    json = {'title': title}
+    url = BASE_URL + '/events'
+    r = requests.post(url, json=json) 
+    assert r.status_code == 201
