86e76f3eef63fafafc59dcb7980b98b356ab14a4
jorgeaber <jorgeaber@gmail.com>
2016-04-12 22:46:30 -0300

    Abstract events tests in class

--- events/test_events.py
+++ events/test_events.py
@@ -1,6 +1,9 @@
 import requests
 from slugify import slugify
+from app import app, db
 import json
+import config
+
 
 BASE_URL = 'http://127.0.0.1:5000'
 
@@ -9,33 +12,52 @@ event_1 = {
     'description': 'This is an event about lorems ipsums'
 }
 
+event_2 = {
+    'title': 'Lorem ipsum dolor sit amet',
+    'description': 'This is an event about lorems ipsums dolors sits amets'
+}
 
-def test_create_event():
-    url = BASE_URL + '/events'
-    r = requests.post(url, json=event_1) 
-    assert r.status_code == 201
+event_3 = {
+    'title': 'I am the master of my fate',
+    'description': 'I am the master of my soul'
+}
 
-def test_get_events_list():
-    url = BASE_URL + '/events'
-    r = requests.get(url) 
-    assert r.status_code == 200
 
-def test_get_event():
-    url = BASE_URL + '/events/' + slugify(event_1['title'], to_lower=True)
-    r = requests.get(url) 
-    assert r.status_code == 200
-    data = json.loads(r.content.decode('utf-8'))
-    assert data['slug'] == slugify(event_1['title'], to_lower=True)
-    assert data['description'] == event_1['description']
 
-def test_get_unexisting_event():
-    url = BASE_URL + '/events/123abc123abc' 
-    r = requests.get(url) 
-    assert r.status_code == 404
+class TestEvent:
 
-def test_delete_event():
-    url = BASE_URL + '/events/' + slugify(event_1['title'], to_lower=True)
-    r = requests.delete(url) 
-    assert r.status_code == 204
-    r = requests.get(url) 
-    assert r.status_code == 404
+    def test_create_events(self):
+        url = BASE_URL + '/events'
+        r = requests.post(url, json=event_1) 
+        assert r.status_code == 201
+        r = requests.post(url, json=event_2) 
+        assert r.status_code == 201
+        r = requests.post(url, json=event_3) 
+        assert r.status_code == 201
+
+    def test_get_events_list(self):
+        url = BASE_URL + '/events'
+        r = requests.get(url) 
+        data = json.loads(r.content.decode('utf-8'))
+        assert len(data['data']) == 3
+        assert r.status_code == 200
+
+    def test_get_event(self):
+        url = BASE_URL + '/events/' + slugify(event_1['title'], to_lower=True)
+        r = requests.get(url) 
+        assert r.status_code == 200
+        data = json.loads(r.content.decode('utf-8'))
+        assert data['slug'] == slugify(event_1['title'], to_lower=True)
+        assert data['description'] == event_1['description']
+
+    def test_get_unexisting_event(self):
+        url = BASE_URL + '/events/123abc123abc' 
+        r = requests.get(url) 
+        assert r.status_code == 404
+
+    def test_delete_event(self):
+        url = BASE_URL + '/events/' + slugify(event_1['title'], to_lower=True)
+        r = requests.delete(url) 
+        assert r.status_code == 204
+        r = requests.get(url) 
+        assert r.status_code == 404
