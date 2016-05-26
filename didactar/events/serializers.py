865c1bb92f2aa6ed4ff6acefb0dc21fa8b5f9045
jorgeaber <jorgeaber@gmail.com>
2016-05-26 01:31:15 -0300

    Add speakers into event serializer

--- didactar/events/serializers.py
+++ didactar/events/serializers.py
@@ -1,12 +1,32 @@
 from flask import jsonify
 
+from didactar.users.models import User
+from didactar.speakerships.models import Speakership
 from didactar.participations.models import Participation
 from didactar.channels.models import Channel
 
 
+def speaker_dict(speakership):
+    user_id = speakership.get_user_id()
+    user = User.get_by_id(user_id)
+    return {
+        'id': speakership.id,
+        'user': {
+            'username': user.username,
+            'name': user.name,
+            'about': user.about,
+            'avatar': user.avatar
+        }
+    }
 
-def channel_dict(channel_id):
-    channel = Channel.get_by_id(channel_id) 
+
+def speakers_dict(event):
+    speakerships = Speakership.filter_by_event(event)
+    return [speaker_dict(s) for s in speakerships]
+
+
+def channel_dict(event):
+    channel = Channel.get_by_id(event.channel_id) 
     return {
         'id': channel.id,
         'slug': channel.slug,
@@ -20,9 +40,10 @@ def event_dict(event):
         'id': event.id, 
         'title': event.title, 
         'slug': event.slug,
-        'channel': channel_dict(event.channel_id),
+        'channel': channel_dict(event),
         'description': event.description,
-        'participationCount': Participation.event_participation_count(event)
+        'participationCount': Participation.event_participation_count(event),
+        'speakers': speakers_dict(event)
     }
 
 
